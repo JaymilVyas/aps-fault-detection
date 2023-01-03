8ff10733c0cf8d872e4c64790072faf6407e5c15
JaymilVyas <jaymilvyas@gmail.com>
2023-01-03 11:40:56 +0530

    Updated

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+def get_requirements():
+    pass
+
+setup(
+    name="sensor",
+    version="0.0.1"
+    author="Jimmy"
+    author_email="jaymilvyas@gmail.command_packages="
+    packages=find_packages(),
+    install_requires=get_requirements(),
+)
